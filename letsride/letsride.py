a3bc34eff98205e797c8d824314d66ffd172df11
andkon <andrew.konoff@gmail.com>
2014-02-16 14:54:49 -0500

    Goddamnit I think it's good now

--- letsride/letsride.py
+++ letsride/letsride.py
@@ -1,1 +1,10 @@
 import tweepy
+
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = '1234abcd...'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = '1234abcd...'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '1234abcd...'#keep the quotes, replace this with your access token
+ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access token secret
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
